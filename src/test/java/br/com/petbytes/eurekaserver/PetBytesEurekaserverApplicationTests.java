79b0f4990c3bb5de8c943d3ba0df57f28915a766
fndribeiro@hotmail.com <012852631@host.docker.internal>
2022-01-19 18:23:42 -0300

    Initial commit.

---
+++ src/test/java/br/com/petbytes/eurekaserver/PetBytesEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petbytes.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBytesEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
